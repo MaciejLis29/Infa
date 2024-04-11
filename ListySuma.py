baedfb0e5e84c6777fe3a276ca4950fcd5fc697c
MaciejLis29 <91671093+MaciejLis29@users.noreply.github.com>
2024-04-11 19:34:36 +0200

    Add files via upload

---
+++ ListySuma.py
@@ -0,0 +1,39 @@
+print("podaj ilość elementów 1 listy")
+i=int(input())
+lista1=[]
+print("podaj ilość elementów 2 listy")
+i2=int(input())
+lista2=[]
+print("podaj elementy 1 listy")
+for a in range(0,i):
+    lista1.append(int(input()))
+print("podaj elementy 2 listy")
+for a in range(0,i2):
+    lista2.append(int(input()))
+if i>i2:
+    temp=lista2
+    for a in range(0,i2):
+        b=lista1[a]
+        c= b in lista2
+        if c==True:
+            temp.remove(b)
+    print("Suma zbiorów: ")
+    print(temp+lista1)
+elif i2>i:
+    temp=lista1
+    for a in range(0,i):
+        b=lista2[a]
+        c=b in lista1
+        if c==True:
+            temp.remove(b)
+    print("Suma zbiorów: ")
+    print(temp+lista2)
+else:
+    temp=lista1
+    for a in range(0, i):
+        b = lista2[a]
+        c = b in lista1
+        if c == True:
+            temp.remove(b)
+    print("Suma zbiorów: ")
+    print(temp+lista2)
